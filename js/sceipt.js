5a191260791e96f9184d6971b15ea02558ee1310
MicheleBenaco <benaco.michele@yahoo.it>
2022-10-19 15:42:13 +0200

    start

---
+++ js/sceipt.js
@@ -0,0 +1,28 @@
+// Descrizione:
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla 
+// volta, i numeri che ha visto precedentemente, tramite una casella di input e un bottone
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei 
+// numeri da indovinare sono stati individuati.
+
+// creare 5 numeri casuali e stamparli sull html
+
+let arrayNumeri = [''];
+const numeriFisso = 5;
+const numeriStampaHtml = document.getElementById('nuemriStampa')
+
+// Funzione che genera randomicamente i numeri 
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+// fare un ciclo, che se i numeri non sono dentro l'array li pusha se no no 
+
+while(arrayNumeri.length < numeriFisso ){
+    getRndInteger(1.100);
+    if(arrayNumeri.includes){
+  const NumeriVisual =  arrayNumeri.push(getRndInteger);
+        console.log(NumeriVisual);
+    
+}
+    }
